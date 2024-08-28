a7db91d001e7f04b81e22606f451a10feb1778f4
hakenshi <fkdias025@gmail.com>
2024-08-27 22:42:38 -0300

    modified:   src/Card/CardRoot.jsx 	modified:   src/Card/CardTimes/CardTime.jsx

--- src/Card/CardTimes/CardTime.jsx
+++ src/Card/CardTimes/CardTime.jsx
@@ -7,9 +7,9 @@ export const CardTime = ({img, nome}) => {
     const foto = img === null || img === "" ? images.timeFoto : img
     const timeNome = nome == null || img === undefined ? "John Doe" : nome
     return (
-        <div className='flex flex-col text-center text-white'>
+        <div className='flex flex-col text-center text-white max-w-32'>
             <img className='rounded-full w-32 border-2 border-collapse' src={foto} alt={timeNome}/>
-            <p className='capitalize py-2'>{nome}</p>
+            <p className='capitalize py-2 truncate'>{nome}</p>
         </div>
     )
 }
